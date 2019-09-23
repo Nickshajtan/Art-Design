eff368d5e0327611afc2cb55b4d0c2b11e49261f
Nickshajtan <shajtanuch@gmail.com>
2019-09-23 12:12:44 +0300

    Update theme-options.php

--- inc/custom-fields-options/theme-options.php
+++ inc/custom-fields-options/theme-options.php
@@ -128,7 +128,7 @@ Container::make( 'theme_options', __('Вставка проивольного к
         Field::make( 'header_scripts', 'crb_header_script' )
             ->set_width(100),
         Field::make("textarea", "body_scripts", __("Body scripts"))
-							->help_text(__("Если в начале тега <body> необходимо добавить скрипты, их нужно указать здеcь."))
+			    ->help_text(__("Если в начале тега <body> необходимо добавить скрипты, их нужно указать здеcь."))
                             ->set_width(100),
         Field::make( 'footer_scripts', 'crb_footer_script' )
             ->set_width(100),
@@ -160,4 +160,54 @@ Container::make( 'theme_options', __('Вставка проивольного к
                                 Field::make( 'checkbox', 'ais', __("Использовать IAS JS?") )
                                      ->set_option_value('yes')
                                      ->set_width(100),
+	    			Field::make( 'text', 'ais_container', __("Использовать IAS JS?") )
+                                     ->set_option_value('yes')
+                                     ->set_width(50)
+	    			     ->help_text(__("Введите classname контейнера для IAS"))
+	    			     ->set_conditional_logic( array(
+						array(
+						    'field' => 'ais',
+						    'value' => true,
+						)
+					) ),
+	    			Field::make( 'text', 'ais_item', __("Использовать IAS JS?") )
+                                     ->set_option_value('yes')
+                                     ->set_width(50)
+	    			     ->help_text(__("Введите classname item-единицы для IAS"))
+	    			     ->set_conditional_logic( array(
+						array(
+						    'field' => 'ais',
+						    'value' => true,
+						)
+					) ),
+	    			Field::make( 'text', 'ais_offset', __("Использовать IAS JS?") )
+                                     ->set_option_value('yes')
+                                     ->set_width(100)
+	    			     ->help_text(__("Количество прокруток прежде чем появится кнопка Load More. По умолчанию равно 1"))
+	    			     ->set_conditional_logic( array(
+						array(
+						    'field' => 'ais',
+						    'value' => true,
+						)
+					) ),
+	    			Field::make( 'text', 'ais_pagination', __("Использовать IAS JS?") )
+                                     ->set_option_value('yes')
+                                     ->set_width(50)
+	    			     ->help_text(__("Введите classname элемента пагинации. Опционально"))
+	    			     ->set_conditional_logic( array(
+						array(
+						    'field' => 'ais',
+						    'value' => true,
+						)
+					) ),
+	    			Field::make( 'text', 'ais_next', __("Использовать IAS JS?") )
+                                     ->set_option_value('yes')
+                                     ->set_width(50)
+	    			     ->help_text(__("Введите classname кнопки 'Дальше' пагинации. Опционально"))
+	    			     ->set_conditional_logic( array(
+						array(
+						    'field' => 'ais',
+						    'value' => true,
+						)
+					) ),
     ));
